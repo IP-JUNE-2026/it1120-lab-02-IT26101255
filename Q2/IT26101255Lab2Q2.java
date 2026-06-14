4b30f9cf156de578266f6eded2c8eb039415fed4
IT26101255 <it26101255@my.sliit.lk>
2026-06-14 22:02:14 +0530

    Add IT26101255Lab2Q2.java to Q2

---
+++ Q2/IT26101255Lab2Q2.java
@@ -0,0 +1,10 @@
+public class IT26101255Lab2Q2 {
+	
+   public static void main(String[] args) {
+	   
+	   double L= 10;
+	   double r= (4*L)/2/3.14;
+	   System.out.println( "radius="+ r);
+   
+   }
+}
