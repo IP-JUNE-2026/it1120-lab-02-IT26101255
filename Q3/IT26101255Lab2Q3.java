0bd194e919d63beee2765ccc9564d6dcb6b23e92
IT26101255 <it26101255@my.sliit.lk>
2026-06-14 22:03:26 +0530

    Add IT26101255Lab2Q3.java to Q3

---
+++ Q3/IT26101255Lab2Q3.java
@@ -0,0 +1,16 @@
+public class IT26101255Lab2Q3 {
+    public static void main( String[] argu) {
+	  double sideA = 3;
+	  double sideB = 4;
+	  double hyotenuse = Math.sqrt(sideA*sideA +sideB*sideB);
+	  System.out.println( "hyotenuse ="+ hyotenuse);
+	}  
+     
+
+
+
+
+
+
+
+}
